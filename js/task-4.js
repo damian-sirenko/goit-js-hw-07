83e6aa5766458c1fcfa519fd93bdce07212b7b85
damian-sirenko <damian.sirenko@gmail.com>
2024-04-13 19:41:15 +0200

    task-3, task-4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,12 @@
-
+const contactForm = document.querySelector('.login-form');
+const onFormSubmit = (event) => {
+	event.preventDefault();
+	const formEl = event.currentTarget.elements;
+	const info = {
+		email: formEl.email.value,
+		password: formEl.password.value,
+	}
+		console.log(info);
+	contactForm.reset();
+}
+contactForm.addEventListener('submit', onFormSubmit);
